10a87eee87439834d785af5d7acb8d9e1fb41376
Xin Tian <xintian89@gmail.com>
2016-04-05 11:41:03 +0800

    Uncomment name portion

--- get_multiple_input.rb
+++ get_multiple_input.rb
@@ -7,7 +7,6 @@ You have chosen: Food Name   Price
 	Total Price: 
 =end
 
-=begin
 puts "What is your first name?"
 first_name = gets.chomp
 
@@ -15,7 +14,6 @@ puts "What is your last name?"
 last_name = gets.chomp
 
 puts "Hello #{first_name} #{last_name}!"
-=end
 
 #must be hard code, its database
 restaurant_menu = Hash.new
